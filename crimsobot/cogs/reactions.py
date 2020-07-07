9fe00f92e1d3b4859519aa65d9bda2bb34123721
garlicOS® <14845347+the-garlic-os@users.noreply.github.com>
2020-07-06 22:27:16 -0500

    Fix fact_add content check
    While more grammatically correct in English, and "or" in this line will let a fact that is missing either its subject or its content (but not both) get added without raising the intended exception. An "and" should produce the intended behavior.

--- crimsobot/cogs/reactions.py
+++ crimsobot/cogs/reactions.py
@@ -88,7 +88,7 @@ class Reactions(commands.Cog):
             await ctx.send(embed=error_embed, delete_after=18)
             return
 
-        if not (fact_subject or fact):
+        if not (fact_subject and fact):
             await ctx.send(embed=error_embed, delete_after=18)
             return
 
